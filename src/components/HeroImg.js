9b4537c13507381317e38faa9706a4dc5b32970d
Goy <saiphanas.k@gmail.com>
2023-08-13 15:59:13 +0800

    feat: HeroImg component

---
+++ src/components/HeroImg.js
@@ -0,0 +1,17 @@
+import "./HeroImgStyles.css";
+
+import React from "react";
+
+// import IntroImg from "../assets/intro-bg.jpg";
+
+function HeroImg() {
+  return (
+    <div className="hero">
+      <div className="mask">
+        {/* <img className="intro-img" src="{IntroImg}" /> */}
+      </div>
+    </div>
+  );
+}
+
+export default HeroImg;
