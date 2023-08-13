73a647ccb851beafd985094b5e694256a92b8938
Goy <saiphanas.k@gmail.com>
2023-08-13 17:09:47 +0800

    fix: add style on HeroImg component

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -2,13 +2,30 @@ import "./HeroImgStyles.css";
 
 import React from "react";
 
-// import IntroImg from "../assets/intro-bg.jpg";
+import IntroImg from "../assets/introbg.jpg";
+
+import { Link } from "react-router-dom";
 
 function HeroImg() {
   return (
     <div className="hero">
       <div className="mask">
-        {/* <img className="intro-img" src="{IntroImg}" /> */}
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>
+          Hello! Im Goy A Software Developer with Fundamental Business Knowledge
+          and Theatre Management background.
+        </p>
+        <h2>Fullstack Developer</h2>
+        <div>
+          <Link to="/project" className="btn">
+            PROJECT
+          </Link>
+          <Link to="/contact" className="btn-light">
+            CONTACT
+          </Link>
+        </div>
       </div>
     </div>
   );
