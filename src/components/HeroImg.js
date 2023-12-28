d56e916792dde2e3331ab2aa596468ec425f69aa
Goy <saiphanas.k@gmail.com>
2023-12-28 18:56:37 +0700

    fix: add more projects

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -1,15 +1,15 @@
-import "./HeroImgStyles.css";
-import React from "react";
-import IntroImg from "../assets/introbg.jpg";
-import { Link } from "react-router-dom";
-import { FaHtml5, FaNode } from "react-icons/fa";
-import { TbBrandJavascript, TbBrandReactNative } from "react-icons/tb";
-import { SiMongodb } from "react-icons/si";
-import { BiLogoPostgresql } from "react-icons/bi";
-import { BiGitBranch } from "react-icons/bi";
-import { TbBrandSupabase } from "react-icons/tb";
-import { SiPostman } from "react-icons/si";
-import { SiJsonwebtokens } from "react-icons/si";
+import './HeroImgStyles.css';
+import React from 'react';
+import IntroImg from '../assets/introbg.jpg';
+import { Link } from 'react-router-dom';
+import { FaHtml5, FaNode } from 'react-icons/fa';
+import { TbBrandJavascript, TbBrandReactNative } from 'react-icons/tb';
+import { SiMongodb } from 'react-icons/si';
+import { BiLogoPostgresql } from 'react-icons/bi';
+import { BiGitBranch } from 'react-icons/bi';
+import { TbBrandSupabase } from 'react-icons/tb';
+import { SiPostman } from 'react-icons/si';
+import { SiJsonwebtokens } from 'react-icons/si';
 
 function HeroImg() {
   return (
@@ -25,39 +25,39 @@ function HeroImg() {
         </p>
         <div className="tech">
           <h3>
-            <FaHtml5 size={50} style={{ color: "#fff", marginRight: "1rem" }} />
+            <FaHtml5 size={50} style={{ color: '#fff', marginRight: '1rem' }} />
             <TbBrandJavascript
               size={50}
-              style={{ color: "#fff", marginRight: "1rem" }}
+              style={{ color: '#fff', marginRight: '1rem' }}
             />
             <TbBrandReactNative
               size={50}
-              style={{ color: "#fff", marginRight: "1rem" }}
+              style={{ color: '#fff', marginRight: '1rem' }}
             />
-            <FaNode size={50} style={{ color: "#fff", marginRight: "1rem" }} />
+            <FaNode size={50} style={{ color: '#fff', marginRight: '1rem' }} />
             <SiMongodb
               size={50}
-              style={{ color: "#fff", marginRight: "1rem" }}
+              style={{ color: '#fff', marginRight: '1rem' }}
             />
             <BiLogoPostgresql
               size={50}
-              style={{ color: "#fff", marginRight: "1rem" }}
+              style={{ color: '#fff', marginRight: '1rem' }}
             />
             <TbBrandSupabase
               size={50}
-              style={{ color: "#fff", marginRight: "1rem" }}
+              style={{ color: '#fff', marginRight: '1rem' }}
             />
             <BiGitBranch
               size={45}
-              style={{ color: "#fff", marginRight: "1rem" }}
+              style={{ color: '#fff', marginRight: '1rem' }}
             />
             <SiPostman
               size={40}
-              style={{ color: "#fff", marginRight: "1rem" }}
+              style={{ color: '#fff', marginRight: '1rem' }}
             />
             <SiJsonwebtokens
               size={40}
-              style={{ color: "#fff", marginRight: "1rem" }}
+              style={{ color: '#fff', marginRight: '1rem' }}
             />
           </h3>
         </div>
@@ -65,16 +65,19 @@ function HeroImg() {
           <Link to="/project" className="btn">
             PROJECTS
           </Link>
-          {/* <Link to="/contact" className="btn-light">
-            DOWNLOAD Resume
-          </Link> */}
-          <a
+          <Link
+            to="https://docs.google.com/document/d/1GxKBEgJEpk7-CRu-KSE7DAFjvrm3SnX8qaKTaQBYuT4/edit?usp=sharing"
+            className="btn-light"
+          >
+            Resume
+          </Link>
+          {/* <a
             href="https://docs.google.com/document/d/1GxKBEgJEpk7-CRu-KSE7DAFjvrm3SnX8qaKTaQBYuT4/edit?usp=sharing"
             download
             className="btn-light"
           >
             RESUME
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,21 +1,28 @@
-import pro1 from "../assets/project1.png";
-import pro2 from "../assets/project2.png";
-// import pro3 from "../assets/project3.png";
+import pro1 from '../assets/project1.png';
+import pro2 from '../assets/project2.png';
+import pro3 from '../assets/project3.png';
 
 const ProjectCardData = [
   {
     imgsrc: pro1,
-    title: "CourseFlow ",
-    text: "An Online Classroom Management System. A platform for teachers to create courses and students to enroll in them.",
-    view: "https://demo-course-flow.vercel.app/",
-    github: "https://github.com/weerayuth9516/course-flow",
+    title: 'CourseFlow ',
+    text: 'An Online Classroom Management System. A platform for teachers to create courses and students to enroll in them.',
+    view: 'https://demo-course-flow.vercel.app/',
+    github: 'https://github.com/weerayuth9516/course-flow',
   },
   {
     imgsrc: pro2,
-    title: "Product Cart",
-    text: "A basic shopping experience, allowing users to select products, adjust quantities, and see the total cost of their selected items.",
-    view: "https://productscart.netlify.app/",
-    github: "https://github.com/gxysai/productcart",
+    title: 'Product Cart',
+    text: 'A basic shopping experience, allowing users to select products, adjust quantities, and see the total cost of their selected items.',
+    view: 'https://productscart.netlify.app/',
+    github: 'https://github.com/gxysai/productcart',
+  },
+  {
+    imgsrc: pro3,
+    title: 'To-do app with NextJS',
+    text: 'This project is a Next.js application that provides CRUD functionality for todo items.',
+    view: 'https://youtu.be/jCRJLZJRYQU',
+    github: 'https://github.com/gxysai/todo-app-nextjs',
   },
 ];
 
